d736f4f363d9f1849be72f112d83cae7bacace9a
Theo MolinaWong <tmolinaw@amazon.com>
2024-08-07 14:42:26 -0700

    Adding original issue comment

--- .github/actions/summarizer/main.js
+++ .github/actions/summarizer/main.js
@@ -4,20 +4,26 @@ const octokit = new github.getOctokit(token);
 const context = github.context;
 
 (async () => {
-  const { data: comments } = await octokit.rest.issues.listComments({
+  const issueContext = {
     owner: context.repo.owner,
     repo: context.repo.repo,
     issue_number: context.issue.number,
-  });
+  };
+
+  const { data: issue } = await octokit.rest.issues.get(issueContext);
+
+  const { data: comments } = await octokit.rest.issues.listComments(issueContext);
 
   let commentLog = "Comment Log:\n";
+
+  commentLog += `${issue.user.login} created the issue:\n "${issue.body}"\n`
 
   for (const comment of comments) {
     if(
       (comment.user.login != "github-actions[bot]") && 
       (!comment.body.startsWith("/"))
     ){
-      commentLog += `${comment.user.login} says:\n"${comment.body}"`
+      commentLog += `${comment.user.login} says:\n"${comment.body}"\n`
     }
   }
 
