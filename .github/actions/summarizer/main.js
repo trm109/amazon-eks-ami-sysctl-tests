daca37409ea59c489696b2ca03606b7ab3e401ff
Theo MolinaWong <tmolinaw@amazon.com>
2024-08-07 14:26:33 -0700

    Moved code to separate file

---
+++ .github/actions/summarizer/main.js
@@ -0,0 +1,25 @@
+const github = require('@actions/github');
+const token = process.env.GITHUB_TOKEN;
+const octokit = new github.getOctokit(token);
+const context = github.context;
+
+(async () => {
+  const { data: comments } = await octokit.rest.issues.listComments({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.issue.number,
+  });
+
+  const commentLog = "Comment Log:\n";
+
+  for (const comment of comments) {
+    commentLog += comment.user.login + " says:\n" + comment.body + "\n"
+  }
+
+  await octokit.rest.issues.createComment({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.issue.number,
+    body: commentLog
+  });
+})();
