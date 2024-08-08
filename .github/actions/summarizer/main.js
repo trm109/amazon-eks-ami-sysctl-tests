8db385b62c30bf65775db05a255a7569ad61dc9b
Theo MolinaWong <tmolinaw@amazon.com>
2024-08-08 16:41:01 -0700

    attempting to separate generation from response

--- .github/actions/summarizer/main.js
+++ .github/actions/summarizer/main.js
@@ -57,6 +57,7 @@ const { BedrockRuntimeClient, InvokeModelCommand } = require("@aws-sdk/client-be
     const responseBody = decoder.decode(response.body);
     console.log(`Raw response:\n${JSON.stringify(response)}`)
     console.log(`decoded response:\n${JSON.stringify(responseBody)}`)
+    console.log(`parsed response:\n${JSON.stringify(responseBody["generation"])}`)
     //const decodedResponseBody = new TextDecoder().decode(apiResponse.body);
     //const responseBody = JSON.parse(decodedResponseBody);
 
