e22902c5ad44fefa878831609802ab7be7fa36a7
Theo MolinaWong <tmolinaw@amazon.com>
2024-08-08 15:52:11 -0700

    First attempt authenticating with AWS OIDC

--- .github/actions/summarizer/main.js
+++ .github/actions/summarizer/main.js
@@ -1,7 +1,12 @@
-const github = require('@actions/github');
-const token = process.env.GITHUB_TOKEN;
+const github  = require('@actions/github');
+const core    = require('@actions/core');
+const token   = process.env.GITHUB_TOKEN;
 const octokit = new github.getOctokit(token);
 const context = github.context;
+import {
+  BedrockRuntimeClient,
+  InvokeModelCommand,
+} from "@aws-sdk/client-bedrock-runtime";
 
 (async () => {
   const issueContext = {
@@ -27,10 +32,36 @@ const context = github.context;
     }
   }
 
+  const client = new BedrockRuntimeClient({ region: process.env.AWS_REGION });
+
+  const prompt = `
+    Give me a short summary of this GitHub Issue reply chain. Include details on what the issue is, and what was the conclusion. The full comment history is below:
+    
+    ${commentLog}
+  `;
+
+  const payload = {
+    prompt,
+    maxTokens: 500,
+    temperature: 0.5,
+  };
+
+  const command = new InvokeModelCommand({
+    contentType: "application/json",
+    body: JSON.stringify(payload),
+    modelId: process.env.MODEL_ID,
+  });
+
+  const apiResponse = await client.send(command);
+
+  // Decode and parse the response body.
+  const decodedResponseBody = new TextDecoder().decode(apiResponse.body);
+  const responseBody = JSON.parse(decodedResponseBody);
+
   await octokit.rest.issues.createComment({
     owner: context.repo.owner,
     repo: context.repo.repo,
     issue_number: context.issue.number,
-    body: commentLog
+    body: responseBody.completions[0].data.text,
   });
 })();
