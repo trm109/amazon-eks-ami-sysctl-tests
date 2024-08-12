c2beffb9453b04e2d680fb8395531cf586c3bb97
Theo MolinaWong <tmolinaw@amazon.com>
2024-08-12 10:40:10 -0700

    removing llama 3 specific inputs

--- .github/actions/summarizer/main.js
+++ .github/actions/summarizer/main.js
@@ -44,7 +44,6 @@ const { BedrockRuntimeClient, InvokeModelCommand } = require("@aws-sdk/client-be
 
   const payload = {
     prompt,
-    max_gen_len: 512,
     temperature: 0.5,
     top_p: 0.9
   };
